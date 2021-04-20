9e011a4e9d1f10f18be5c13c2a39330b6705fabe
yangkui <c6swift@gmail.com>
2021-04-20 11:39:34 +0800

    修复几个问题，补全单测

--- tests/lib/rules/lazy-route.test.js
+++ tests/lib/rules/lazy-route.test.js
@@ -13,7 +13,44 @@ const expectedError = (componentName) => ({
 ruleTester.run('test rule', rule, {
   valid: [
     {
-      code: `'中文'`,
+      code: `import React, { FC } from 'react';
+import { Switch, Route, useRouteMatch } from 'react-router-dom';
+const DevIndex = React.lazy(() =>
+  import('./DevIndex').then((module) => ({ default: module.DevIndex }))
+);
+const DevDetails = React.lazy(() =>
+  import('./DevDetails').then((module) => ({ default: module.DevDetails }))
+);
+const DevDetailEdit = React.lazy(() => import('./DevDetailEdit'));
+const DevAtomDebug = React.lazy(() =>
+  import('./DevAtomDebug').then((module) => ({ default: module.DevAtomDebug }))
+);
+
+export const isDev = process.env.NODE_ENV === 'development';
+
+const AtomDevelop = () => {
+  const { path } = useRouteMatch();
+
+  return (
+    <Switch>
+      <Route  exact>
+        <DevIndex />
+      </Route>
+      <Route  exact>
+        <DevDetails />
+      </Route>
+      <Route  exact>
+        <DevDetailEdit />
+      </Route>
+      <Route  exact>
+        <DevAtomDebug />
+      </Route>
+    </Switch>
+  );
+};
+
+export default AtomDevelop;
+`,
     },
   ].map(parserMap),
   invalid: [
@@ -60,11 +97,11 @@ export default AtomDevelop;
       code: `
       import React, { FC } from 'react';
 import { Switch, Route, useRouteMatch } from 'react-router-dom';
-// import { DevIndex } from './DevIndex';
-// import { DevDetails } from './DevDetails';
-import DevDetailEdit from './DevDetailEdit';
+import { DevIndex } from './DevIndex';
+import { DevDetails } from './DevDetails';
 
 const DevAtomDebug = React.lazy(() => import('./DevAtomDebug').then(module => module.DevAtomDebug));
+const DevDetailEdit = React.lazy(() => import('./DevDetailEdit'));
 
 export const isDev = process.env.NODE_ENV === 'development';
 
@@ -89,11 +126,7 @@ const AtomDevelop = () => {
 
 export default AtomDevelop;
 `,
-      errors: [
-        expectedError('DevIndex'),
-        expectedError('DevDetails'),
-        expectedError('DevDetailEdit'),
-      ],
+      errors: [expectedError('DevIndex'), expectedError('DevDetails')],
     },
   ].map(parserMap),
 });
