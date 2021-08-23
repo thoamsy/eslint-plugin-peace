9065a05746a955cdc91a6be2fb874ad020671cff
yangkui <c6swift@gmail.com>
2021-08-23 15:33:24 +0800

    refactor: 改进 options，不需要再写额外的 key

--- tests/lib/rules/brand-typo.test.js
+++ tests/lib/rules/brand-typo.test.js
@@ -14,15 +14,15 @@ ruleTester.run('test rule', rule, {
   valid: [
     {
       code: `const str = 'TikTok'`,
-      options: [{ brands: 'TikTok' }],
+      options: ['TikTok'],
     },
     {
       code: `if (true) { str = 'TikTok' } else { str = 'douyin' }`,
-      options: [{ brands: 'TikTok' }],
+      options: ['TikTok'],
     },
     {
       code: `<div>Call me in TikTok</div>`,
-      options: [{ brands: 'TikTok' }],
+      options: ['TikTok'],
     },
   ].map(parserMap),
   invalid: [
@@ -30,32 +30,31 @@ ruleTester.run('test rule', rule, {
       code: `const str = 'Tiktok'`,
       errors: [expectedError()],
       output: `const str = 'TikTok'`,
-      options: [{ brands: 'TikTok' }],
+      options: ['TikTok'],
     },
     {
       code: `if (true) { str = 'tiktok' }`,
       errors: [expectedError()],
       output: `if (true) { str = 'TikTok' }`,
-      options: [{ brands: 'TikTok' }],
+      options: ['TikTok'],
     },
-
     {
       code: `<div>call me in tiktok</div>`,
       errors: [expectedError(types.JSXText)],
       output: `<div>call me in TikTok</div>`,
-      options: [{ brands: 'TikTok' }],
+      options: ['TikTok'],
     },
     {
       code: `'请问你是ios手机还是android手机'`,
       errors: [expectedError()],
       output: `'请问你是iOS手机还是Android手机'`,
-      options: [{ brands: ['iOS', 'Android'] }],
+      options: ['iOS', 'Android'],
     },
     {
       errors: [expectedError()],
       code: `'请问你是iOS手机还是Android手机'`,
       output: `'请问你是ios手机还是android手机'`,
-      options: [{ brands: ['ios', 'android'] }],
+      options: [['ios', 'android']],
     },
   ].map(parserMap),
 });
