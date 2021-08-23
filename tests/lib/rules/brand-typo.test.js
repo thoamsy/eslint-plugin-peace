3c98e6fdc2374799b56cd7fd4d16f14d3ba16d63
yangkui <c6swift@gmail.com>
2021-08-23 15:19:18 +0800

    feat: 支持检测品牌名的拼写

---
+++ tests/lib/rules/brand-typo.test.js
@@ -0,0 +1,61 @@
+const { RuleTester } = require('eslint');
+const parserMap = require('./parserOptions');
+const types = require('../../../lib/util/types');
+const rule = require('../../../lib/rules/brand-typo');
+
+const ruleTester = new RuleTester();
+
+const expectedError = (type = types.Literal) => ({
+  message: 'Your got a typo for your brand',
+  type,
+});
+
+ruleTester.run('test rule', rule, {
+  valid: [
+    {
+      code: `const str = 'TikTok'`,
+      options: [{ brands: 'TikTok' }],
+    },
+    {
+      code: `if (true) { str = 'TikTok' } else { str = 'douyin' }`,
+      options: [{ brands: 'TikTok' }],
+    },
+    {
+      code: `<div>Call me in TikTok</div>`,
+      options: [{ brands: 'TikTok' }],
+    },
+  ].map(parserMap),
+  invalid: [
+    {
+      code: `const str = 'Tiktok'`,
+      errors: [expectedError()],
+      output: `const str = 'TikTok'`,
+      options: [{ brands: 'TikTok' }],
+    },
+    {
+      code: `if (true) { str = 'tiktok' }`,
+      errors: [expectedError()],
+      output: `if (true) { str = 'TikTok' }`,
+      options: [{ brands: 'TikTok' }],
+    },
+
+    {
+      code: `<div>call me in tiktok</div>`,
+      errors: [expectedError(types.JSXText)],
+      output: `<div>call me in TikTok</div>`,
+      options: [{ brands: 'TikTok' }],
+    },
+    {
+      code: `'请问你是ios手机还是android手机'`,
+      errors: [expectedError()],
+      output: `'请问你是iOS手机还是Android手机'`,
+      options: [{ brands: ['iOS', 'Android'] }],
+    },
+    {
+      errors: [expectedError()],
+      code: `'请问你是iOS手机还是Android手机'`,
+      output: `'请问你是ios手机还是android手机'`,
+      options: [{ brands: ['ios', 'android'] }],
+    },
+  ].map(parserMap),
+});
